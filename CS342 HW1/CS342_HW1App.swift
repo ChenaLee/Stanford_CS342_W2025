b7a4987d00632e1d1a29d2230e127b861d2a33a0
Chena Lee <chenaclee@gmail.com>
2025-01-08 22:36:59 -0800

    Initial Commit

---
+++ CS342 HW1/CS342_HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  CS342_HW1App.swift
+//  CS342 HW1
+//
+//  Created by Chena Lee on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CS342_HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS342 HW1Tests/CS342_HW1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  CS342_HW1Tests.swift
+//  CS342 HW1Tests
+//
+//  Created by Chena Lee on 1/8/25.
+//
+
+import Testing
+@testable import CS342_HW1
+
+struct CS342_HW1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
